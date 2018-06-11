f21826cc458d6ecf31d94bc034146ac99a125d60
Lucas Rowe <lucas.rowe.public@gmail.com>
2018-06-10 23:09:00 -0400

    Add hyper keys for my personal apps.
    Removed the hyper key overrides file from gitignore because I'm using this repo as a backup for my machine.

---
+++ hammerspoon/hyper-apps.lua
@@ -0,0 +1,14 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 'b', 'Google Chrome' },     -- "B" for "Browser"
+  { 'c', 'Slack' },             -- "C for "Chat"
+  { 'e', 'Atom' },              -- "E" for "Editor"
+  { 'f', 'Finder' },            -- "F" for "Finder"
+  { 's', 'Spotify' },           -- "S" for "Spotify"
+  { 't', 'iTerm' },             -- "T" for "Terminal"
+  { 'u', 'Unity' },             -- "U" for "Unity"
+}
